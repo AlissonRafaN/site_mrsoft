bc5d3772354d1f8034250434871d33dbeaf980f5
AlissonRafaN <alissonrafan@gmail.com>
2022-01-06 14:16:41 -0300

    Criação de banco de dados e schemas projeto.

--- __init__.py
+++ __init__.py
@@ -6,6 +6,22 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='DEV'
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        SECRET_KEY='DEV',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return "<p>Hello, World!</p>"
+
+    return app
