3a462f7685cf965ed6c99063a14272341581fd73
WelberthTito <welberthvieiratito@gmail.com>
2022-01-06 10:37:29 -0300

    Criada uma nova rota: Produtos.

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def hello_world():
     return "<p>Hello World</p>"
 
+
 @app.route("/sobre")
 def sobre():
-    return "<h1>Revenda de sisstemas para o seu estabelecimento</h1>"
+    return "<h1>Revenda de sistemas para o seu estabelecimento</h1>"
+
 
 @app.route("/contato")
 def contato():
-    return
+    return ""
+
+
+@app.route("/produtos")
+def produtos():
+    return ""
