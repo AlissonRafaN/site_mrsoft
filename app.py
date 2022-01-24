f8100a7b64743abed2f97bda6f5ba242a46ef2cc
Alisson Rafael <alissonrafan@gmail.com>
2022-01-24 16:49:23 -0300

    Atualização app.py, index.html e form.html
    Co-authored-by: Welberth  Tito  <WelberthTito@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message 
+import smtplib
 
 app = Flask(__name__)
 
@@ -9,3 +12,21 @@ def homepage():
 if __name__ == "__main__":
     app.run(debug=True)
     app.debug = True
+
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact(): 
+    name = request.form.get('nome')
+    email = request.form.get('email')
+    assunto = request.form.get('assunto')
+    mensagem = request.form.get('mensagem')
+
+
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login("welberthvieiratito@gmail.com", "We30819099")
+    server.sendmail("welberthvieiratito@gmail.com", email, assunto, name, mensagem,) 
+    title = "Agradecemos o Contato!"
+    return render_template("index.html", title=title, name=name, email=email, assunto=assunto, mensagem= mensagem)
+    
