38c11edb047fb9620fc0fbfe95699099e16df14d
Alisson Rafael <alissonrafan@gmail.com>
2022-01-31 14:24:15 -0300

    Testes com formulário

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ import os
 from flask import Flask, render_template, request
 from email.message import EmailMessage
 import smtplib
+import email
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
+
+
+csrf = CSRFProtect()
+
+class form_email(FlaskForm):
+    name = StringField('Nome', validators=[DataRequired('Nome não pode ficar vazio')])
+    email = StringField('E-mail', validators=[DataRequired('E-mail não pode ficar vazio')])
+    subject = StringField('Assunto', validators=[DataRequired('Assunto não pode ficar vazio')])
+    message = TextAreaField('Mensagem', validators=[DataRequired('Mensagem não pode ficar vazio')])
 
 
 app = Flask(__name__)
@@ -11,23 +24,13 @@ def homepage():
     return render_template("index.html")
 
 
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form = form_email()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
 
-@app.route('/contact', methods=['POST', 'GET'])
-def contact(): 
-    msg = EmailMessage()
-
-
-    msg['Subject']=request.form.get('assunto')
-    msg['From']='Contato Mr.Soft'
-    msg['To']='welberthvieiratito@gmail.com'
-    server = smtplib.SMTP("smtp.gmail.com",587)
-    server.starttls()
-    server.login("welberthvieiratito@gmail.com", "")
-    server.send_message(msg) 
-    title = "Agradecemos o Contato!"
-    server.quit() 
-    return render_template("index.html")
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
